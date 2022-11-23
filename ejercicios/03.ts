bf2ecabaf4af7e206a928e4c29712f1250b04f91
jdmaradiago2000 <95006922+jdmaradiago2000@users.noreply.github.com>
2022-11-23 13:40:27 -0500

    1 file modified, 2 added

---
+++ ejercicios/03.ts
@@ -0,0 +1,13 @@
+/*
+  ===== Código de TypeScript =====
+*/
+
+function sumar(a: number, b: number) {
+  return a + b;
+}
+
+const resultado = sumar(5, 7);
+
+console.log(resultado);
+
+console.log('Mostrar mensaje en consola');
