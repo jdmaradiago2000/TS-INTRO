29534591c4940dd178294efa2c7c9d1985f62990
jdmaradiago2000 <95006922+jdmaradiago2000@users.noreply.github.com>
2022-11-24 11:37:58 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
 /*
   ===== Código de TypeScript =====
 */
+
+const superHero = {
+  nombre: 'Spiderman',
+  edad: 30,
+  dirección: {
+    calle: 'Main st',
+    pais: 'USA',
+    ciudad: 'New York',
+  },
+  mostrarDireccion() {
+    return (
+      this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais
+    );
+  },
+};
+
+const direccion = superHero.mostrarDireccion();
+console.log(direccion);
