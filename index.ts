b39db8efb5b6e42f4ba4eb5e2c631290ad4a1256
jdmaradiago2000 <95006922+jdmaradiago2000@users.noreply.github.com>
2022-11-24 12:20:04 -0500

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,34 +1,3 @@
 /*
   ===== Código de TypeScript =====
 */
-
-interface Direccion {
-  calle: string;
-  pais: string;
-  ciudad: string;
-}
-
-interface SuperHeroe {
-  nombre: string;
-  edad: number;
-  direccion: Direccion;
-  mostrarDireccion();
-}
-
-const superHeroe: SuperHeroe = {
-  nombre: 'Spiderman',
-  edad: 30,
-  direccion: {
-    calle: 'Main st',
-    pais: 'USA',
-    ciudad: 'New York',
-  },
-  mostrarDireccion() {
-    return (
-      this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais
-    );
-  },
-};
-
-const direccion = superHeroe.mostrarDireccion();
-console.log(direccion);
