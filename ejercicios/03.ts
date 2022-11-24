7b863658f08fc3de49354f6cc4337077b3a562a9
jdmaradiago2000 <95006922+jdmaradiago2000@users.noreply.github.com>
2022-11-24 09:29:53 -0500

    1 file modified

--- ejercicios/03.ts
+++ ejercicios/03.ts
@@ -6,6 +6,14 @@ function sumar(a: number, b: number) {
   return a + b;
 }
 
+function multiplicar(
+  numero1: number,
+  numero2?: number,
+  base: number = 2
+): number {
+  return numero1 * base;
+}
+
 const resultado = sumar(5, 7);
 
 console.log(resultado);
