db025a83697d918178b161903db38d0de7db1b1a
jdmaradiago2000 <95006922+jdmaradiago2000@users.noreply.github.com>
2022-11-26 18:55:45 -0500

    2 files modified

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 /*
   ===== Código de TypeScript =====
 */
+
+import { Producto } from './ejercicios/06-desestructuracion-funcion';
+import { calculaISV } from './ejercicios/06-desestructuracion-funcion';
+
+const carritoCompras: Producto[] = [
+  {
+    desc: 'Telefono 1',
+    precio: 100,
+  },
+  {
+    desc: 'Telefono 2',
+    precio: 150,
+  },
+];
+
+const [total, isv] = calculaISV(carritoCompras);
+
+console.log('Total', total);
+console.log('ISV', isv);
